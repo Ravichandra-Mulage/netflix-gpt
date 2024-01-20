db4a1cbe53745b92e0213640b15b21b6a3fe4f84
Ravichandra Mulage <ravichandramulage00@gmail.com>
2024-01-20 20:43:49 +0530

    Secondary Container with movie List && usePopular Movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,14 +3,24 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
   return (
     <div>
       <Header />
       <MainContainer/>
       <SecondaryContainer/>
+       {/* 
+          MainContainer
+            - VideoBackground
+            - VideoTitle
+          SecondaryContainer
+            - MovieList * n
+              - cards * n
+      */}
     </div>
   );
 };
